042cf0eddc3733af76c6ecaecab779bcd3244dc7
Liana Sabino Alves <liana.sabino@matriz.fornecedoramaquinas.com.br>
2022-12-01 11:14:44 -0300

    Fim do html/css

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,6 +1,12 @@
+import { FiPlus } from 'react-icons/fi';
+
 import { Container, Brand, Menu, Search, Content, NewNote } from './styles';
 
-import { Header } from '../../components/Header'
+import { Header } from '../../components/Header';
+import { Note } from '../../components/Note';
+import { Input } from '../../components/Input';
+import { Section } from '../../components/Section';
+import { ButtonText } from '../../components/ButtonText';
 
 export function Home(){
     return(
@@ -12,19 +18,34 @@ export function Home(){
             <Header />
 
             <Menu>
+                <li><ButtonText title="Todos" isActive/></li>
+                <li><ButtonText title="React" /></li>
+                <li><ButtonText title=" NodeJs" /></li>
 
             </Menu>
 
             <Search>
+                <Input placeholder="Pesquisar pelo título" />
 
             </Search>
 
             <Content>
+                <Section title="Minhas notas">
+                    <Note data={{
+                        title: 'React', 
+                        tags:[
+                            { id: '1', name: 'react'},
+                            {id: '2', name: 'rocketseat'}
+                        ]
+                    }}
+                    />
+                </Section>
 
             </Content>
 
             <NewNote>
-
+                <FiPlus />
+                Criar nota
             </NewNote>
 
         </Container>
