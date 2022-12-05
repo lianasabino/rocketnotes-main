7c8c8bc37e2f3f1174770282bcd3f2a92ebed7ed
Liana Sabino Alves <liana.sabino@matriz.fornecedoramaquinas.com.br>
2022-12-05 13:04:27 -0300

    Configurando pages

--- src/main.jsx
+++ src/main.jsx
@@ -5,13 +5,13 @@ import GlobalStyle from './styles/global';
 
 import theme from './styles/theme'
 
-import { Home } from './pages/Home';
+import { New } from './pages/New';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyle />
-      <Home />
+      <New />
     </ThemeProvider>
   </React.StrictMode>
 )
